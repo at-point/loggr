30d2f39fa125a48b997cd89e50082d4beb6ba7cd
lukas <lukas.westermann@gmail.com>
2011-07-02 16:03:38 +0200

    added NOP logger which does not log anything at all

---
+++ lib/loggr/adapter/nop.rb
@@ -0,0 +1,39 @@
+require 'loggr/adapter/abstract'
+
+module Loggr
+  module Adapter
+
+    # Silences all logging operations, nothing is written at all.
+    #
+    class NOPAdapter < AbstractAdapter
+      
+      class NOPLogger
+        # Has no impact anyway :)
+        attr_accessor :level
+        
+        # Just to ensure compatiability with AS::BufferedLogger
+        attr_reader :auto_flushing, :flush, :close
+        
+        # Yields empty implementations for all severities
+        %w{trace debug info warn error fatal}.each do |severity|
+          class_eval <<-EOT, __FILE__, __LINE__ + 1
+            def #{severity}(*args, &block)   # def debug(*args, &block)
+            end                              # end
+
+            def #{severity}?                 # def debug?
+              false                          #   false
+            end                              # end
+          EOT
+        end
+      end
+      
+      # Get single NOPLogger instance
+      def logger(name, options = {})
+        @logger ||= NOPLogger.new
+      end      
+    end
+    
+    # THE instance
+    NOP = NOPAdapter.new
+  end
+end
