28233c83f5dfdf368b129d3a6cac48fbdde784f0
lukas <lukas.westermann@gmail.com>
2011-07-04 11:17:03 +0200

    skip Lint tests when not running via jruby

--- test/unit/adapter/slf4j_test.rb
+++ test/unit/adapter/slf4j_test.rb
@@ -6,10 +6,15 @@ class Loggr::Adapter::SLF4JTest < MiniTest::Unit::TestCase
   def setup
     java do
       require 'loggr/adapter/slf4j'
-      self.class.send(:include, Loggr::Lint::Tests)
       @adapter = Loggr::Adapter::SLF4JAdapter.new
     end
   end
+
+  def test_lint
+    skip_unless_java
+  end
+    
+  include(Loggr::Lint::Tests) if java?
 
   def test_slf4j_should_be_a_slf4j_adapter
     skip_unless_java
