864b119f1e69042f70b63a9d1c325f030e8cae96
lukas <lukas.westermann@gmail.com>
2011-10-24 11:13:43 +0200

    fix whitespace

--- lib/loggr/slf4j/logger.rb
+++ lib/loggr/slf4j/logger.rb
@@ -1,12 +1,13 @@
 require 'loggr/severity'
+require 'logger/slf4j/mdc'
 
 module Loggr
   module SLF4J
-    
+
     # Simple marker factory which uses `org.slf4j.MarkerFactory`, but
     # caches the result in a local ruby hash, by name.
     class MarkerFactory
-      
+
       # Get marker for any non-empty string.
       def self.[](name)
         name = name.to_s.strip
@@ -15,35 +16,35 @@ module Loggr
         @markers[name] ||= Java::OrgSlf4j::MarkerFactory.getMarker(name)
       end
     end
-    
+
     # A logger which is backed by SLF4J, thus only useable in a JRuby environment.
     #
     class Logger
-      
+
       # Get severities
       include Loggr::Severity
-      
+
       # Basically has *no* impact, because is handled by SLF4J
       attr_accessor :level
-      
+
       # Just to ensure compatiability with AS::BufferedLogger
       attr_reader :auto_flushing, :flush, :close
-      
+
       # Access raw SLF4J logger & marker instances
       attr_reader :java_logger, :java_marker
-      
+
       # Create a new Logger instance for the given name
       #
       def initialize(name, options = {})
         name = self.class.in_java_notation(name)
         @java_logger = Java::OrgSlf4j::LoggerFactory.getLogger(name.to_s)
         @java_marker = MarkerFactory[options[:marker]]
-        
+
         # seriously, this is handled by slf4j and pretty dynamic
         @level = Logger::UNKNOWN
         @auto_flushing = true
       end
-      
+
       # Create the logger methods via meta programming, sweet.
       %w{trace debug info warn error}.each do |severity|
         class_eval <<-EOT, __FILE__, __LINE__ + 1
@@ -53,7 +54,7 @@ module Loggr
               java_logger.#{severity}(marker, build_message(message, progname, &block))  #     java_logger.debug(marker, build_message(message, progname, &block))
             end                                                                          #   end
           end                                                                            # end
-          
+
           def #{severity}?                                                               # def debug?
             !!java_logger.is_#{severity}_enabled(java_marker)                            #   !!java_logger.is_debug_enabled(java_marker)
           end                                                                            # end
@@ -63,7 +64,7 @@ module Loggr
       # Add support for fatal, just alias to error
       alias_method :fatal, :error
       alias_method :fatal?, :error?
-      
+
       # If a class, module or object is used converts `Foo::Bar::SomeThing` to
       # java notation: `foo.bar.SomeThing`. Symbols and Strings are left as is!
       #
@@ -74,15 +75,15 @@ module Loggr
         last  = parts.pop
         parts.map { |p| p.downcase }.push(last).join('.')
       end
-            
+
       protected
-        
+
         # Construct the message, note that progname will be ignored, maybe set as
         # MDC?
         def build_message(message = nil, progname = nil, &block)
           message = yield if message.nil? && block_given?
           message.to_s.gsub(/$\s*^/, '')
-        end        
+        end
     end
   end
 end
